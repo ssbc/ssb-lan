743f583d9885dfe901c8b8040fbc08a26e89e6c5
Andre Staltz <andre@staltz.com>
2019-07-19 19:00:41 +0300

    add better tests

--- test/index.js
+++ test/index.js
@@ -1,19 +1,39 @@
 const tape = require('tape');
 const Keys = require('ssb-keys');
+const broadcast = require('broadcast-stream');
+const port = require('../port');
 
 const createSsbServer = require('ssb-server').use(require('../lib/index'));
 
 tape('broadcasting looks correct', t => {
-  var alice = createSsbServer({
+  t.plan(7);
+
+  const keys = Keys.generate();
+  const alice = createSsbServer({
     temp: 'test-lan-alice',
     timeout: 1000,
     port: 8008,
-    keys: Keys.generate(),
+    keys,
   });
   alice.lan.start();
 
-  setTimeout(() => {
+  const b = broadcast(port);
+  b.on('data', buf => {
+    const msg = JSON.parse(buf.toString());
+
+    t.equals(typeof msg.address, 'string', 'address is a string');
+    t.ok(msg.address, 'address is okay');
+    t.equals(msg.address.slice(0, 4), 'net:', 'address begins with net:');
+
+    t.equals(typeof msg.capsHash, 'string', 'capsHash is a string');
+    t.ok(msg.capsHash, 'capsHash is okay');
+
+    t.equals(typeof msg.signature, 'string', 'signature is a string');
+    t.ok(msg.signature, 'signature is okay');
+
+    b.close();
     alice.lan.stop();
+    alice.close();
     t.end();
-  }, 10e3);
+  });
 });
