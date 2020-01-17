4f3af0d466697473909fb90dbcfec584d956f408
Andre Staltz <andre@staltz.com>
2020-01-17 23:50:31 +0200

    update tests

--- test/index.js
+++ test/index.js
@@ -7,8 +7,8 @@ const Ref = require('ssb-ref');
 
 const createSsbServer = require('ssb-server').use(require('../lib/index'));
 
-tape.skip('(LEGACY) broadcasting looks correct', t => {
-  t.plan(7);
+tape('Legacy (when enabled) broadcasting looks correct', t => {
+  t.plan(1);
 
   const keys = Keys.generate();
   const alice = createSsbServer({
@@ -19,24 +19,18 @@ tape.skip('(LEGACY) broadcasting looks correct', t => {
   });
   alice.lan.start();
 
-  const b = broadcast(port);
+  const b = broadcast(8008);
   b.on('data', buf => {
-    const msg = JSON.parse(buf.toString());
+    const msg = buf.toString();
 
-    t.equals(typeof msg.address, 'string', 'address is a string');
-    t.ok(msg.address, 'address is okay');
-    t.equals(msg.address.slice(0, 4), 'net:', 'address begins with net:');
-
-    t.equals(typeof msg.capsHash, 'string', 'capsHash is a string');
-    t.ok(msg.capsHash, 'capsHash is okay');
-
-    t.equals(typeof msg.signature, 'string', 'signature is a string');
-    t.ok(msg.signature, 'signature is okay');
+    t.true(Ref.isAddress(msg), 'broadcasted content is a multiserver address');
 
     b.close();
     alice.lan.stop();
     alice.close();
-    t.end();
+    setTimeout(() => {
+      t.end();
+    }, 1000);
   });
 });
 
@@ -79,7 +73,10 @@ tape('broadcast write is correct', t => {
     b.close();
     alice.lan.stop();
     alice.close();
-    t.end();
+
+    setTimeout(() => {
+      t.end();
+    }, 1000);
   });
 });
 
@@ -111,7 +108,10 @@ tape('broadcast read is correct', t => {
       b.close();
       alice.lan.stop();
       alice.close();
-      t.end();
+
+      setTimeout(() => {
+        t.end();
+      }, 1000);
     }),
   );
 
